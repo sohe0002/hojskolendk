a74bc61a2b4536841370629c11840b48c67c31de
Sarah Albaek <saal0002@stud.kea.dk>
2024-11-27 09:29:45 +0100

    opdatering

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -115,7 +115,7 @@ h3_desk: ['28px', { lineHeight: '3rem' }],
 h2_desk: ['32px', { lineHeight: '3.75rem' }],
 h1_desk: ['64px', { lineHeight: '4.5rem' }],
 
-p_mobil: ['12px', { lineHeight: '1.25rem' }],
+p_mobil: ['14px', { lineHeight: '1.25rem' }],
 h4_mobil: ['14px', { lineHeight: '2.25rem' }],
 h3_mobil: ['17px', { lineHeight: '3rem' }],
 h2_mobil: ['18px', { lineHeight: '' }],
